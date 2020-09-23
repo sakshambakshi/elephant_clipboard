9435936a7377febac24aa36f1203f20aa918ac9c
sakshambakshi <sakshambakshi@gmail.com>
2020-09-23 17:13:14 +0530

    :saksham display clipboard on click getting copied and added sound

--- renderer.js
+++ renderer.js
@@ -1,30 +1,72 @@
 const Vue = require("vue/dist/vue.js")
 const electron = require("electron")
-const {clipboard} = electron
+const { clipboard } = electron
 
-function getCurentCopiedTxt(){
+function getCurentCopiedTxt() {
     return clipboard.readText();
 }
 // console.log(readClipboardText())
-
+const audio = new Audio("./sounds/elephantcub.mp3")
 
 const App = new Vue({
-    el:"#app",
-    data:{
-        title:"🐘 Elephant Clipboard 🎉",
-        history:[]
+    el: "#app",
+    data: {
+        title: "🐘 Elephant Clipboard 🎉",
+        history: []
     },
-    mounted(){
-        setInterval(this.checkClipboard , 500)
+    mounted() {
+        const text = getCurentCopiedTxt()
+        
+        this.history.unshift({
+            text,
+            type:"",
+            clipped: new Date()
+        })
+        setInterval(this.checkClipboard, 500)
     },
-    methods:{
-        checkClipboard   (){
+    computed: {
+        historyReversed() {
+          return this.history.slice().reverse();
+        }
+      },
+    methods: {
+        checkClipboard() {
             const text = getCurentCopiedTxt()
             // console.log(this.history.length)
             // if(this.history.length){
-                this.history[0] !== text ? this.history.unshift(text) : "" 
+            // if(this.history.length == 0){
+            //     this.history.unshift({
+            //         text,
+            //         clipped: new Date() 
+            //     })
             // }
-            console.log(this. history)
+            // else{
+           if(this.history[this.history.length - 1]["text"] !== text) { 
+               this.history.push({
+                text,
+                clipped: new Date(),
+                isClicked:false
+            }) 
+            // alert(this.history.length);
+            
+        }
+            // }
+
+            // this.history.indexOf(text) ==    -1 ? this.history.unshift(text) : "" 
+
+            // }
+            console.log(this.history)
+        },
+        itemClick(item , index) {
+            
+            this.history.splice(this.history.indexOf(item) , 1);
+            
+            audio.currentTime = 0 
+            audio.play()
+            clipboard.writeText(item.text)
+        
+            alert(item.text)
         }
     }
 })
+
